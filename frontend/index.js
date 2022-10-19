46c879bbf8ca034c8dc119b6fc14e9d459f0f4a1
juaovitu13 <joao.victorsoares@outlook.com>
2022-10-19 14:27:42 -0300

    Atualização

--- frontend/index.js
+++ frontend/index.js
@@ -17,11 +17,9 @@ function criaLinha(sala) {
 
     //Cria a coluna com o botão para excluir
     tdExcluir = document.createElement("td");
-    
-    //Voc~e néo copiou o código que te mandei naõ uai. Tõ totalmente diferente
-    //tdExcluir.innerHTML = '<button onclick="excluirSala()' + tdId.textContent + ')">Deletar</button>'
     tdExcluir.innerHTML = '<button type="button" class="btn btn-danger" onclick="excluirSala(' + tdId.textContent + ')">Deletar</button>';
-
+    tdExcluir.setAttribute("onclick", "id.excluirSala()");
+    
     //Adiciona as colunas na linha
     linha.appendChild(tdId);
     linha.appendChild(tdNome);
@@ -33,6 +31,9 @@ function criaLinha(sala) {
 
 function excluirSala(id) {
     if (confirm("Tem certeza que deseja excluir a sala de id: " + id + "?") == true) {
+        let data = fazDelete("http://127.0.0.1:8000/salas/id");
+        let tabela = document.getElementById("tabela");
+        tabela.deleteRow(i);
         // Aqui você inclui a chamada para a API, usando o verbo DELETE do http
         // Depois exibe a mensagem de sucesso e atualiza a página de salas, para remover o item excluido da grade
     }
