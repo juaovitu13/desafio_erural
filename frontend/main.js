9b80e43d9dce55c84128ba37f8798aa97ea1c379
juaovitu13 <joao.victorsoares@outlook.com>
2022-10-19 12:10:33 -0300

    Atualização

--- frontend/main.js
+++ frontend/main.js
@@ -10,23 +10,34 @@ function criaLinha(sala) {
     linha = document.createElement("tr");
     tdId = document.createElement("td");
     tdNome = document.createElement("td");
+
+    //Cria as duas colunas com os dados das salas
     tdId.innerHTML = sala.id
     tdNome.innerHTML = sala.nome
 
+    //Cria a coluna com o botão para excluir
+    tdExcluir = document.createElement("td");
+    tdExcluir.innerHTML = '<button onclick="excluirSala(' + tdId.textContent + ')">Deletar</button>'
+
+    //Adiciona as colunas na linha
     linha.appendChild(tdId);
     linha.appendChild(tdNome);
+    linha.appendChild(tdExcluir);
+
 
     return linha;
 }
 
-//Abre o formulário de cadastro de salas
-function abrirFormCadastro(){
-    window.open('cadastro/form.html');
+function excluirSala(id) {
+    if (confirm("Tem certeza que deseja excluir a sala de id: " + id + "?") == true) {
+        // Aqui você inclui a chamada para a API, usando o verbo DELETE do http
+        // Depois exibe a mensagem de sucesso e atualiza a página de salas, para remover o item excluido da grade
+    }
 }
 
-//deletar salas
-function abrirFormDeletar(){
-    window.open('excluir/delet.html');
+//Abre o formulário de cadastro de salas
+function abrirFormCadastro() {
+    window.location.href ='cadastro/form.html'
 }
 
 function main() {
@@ -36,11 +47,11 @@ function main() {
     salas.forEach(element => {
         let linha = criaLinha(element);
         tabela.appendChild(linha);
-    });
+        });
 
-   // Para cada sala
+        // Para cada sala
         // criar uma linha
         // adicionar na tabela
-}
+    }
 
-main()
+    main()
